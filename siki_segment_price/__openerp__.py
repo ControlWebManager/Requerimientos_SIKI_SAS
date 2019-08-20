7227af302cccb0aaad1c571ed20ea328c4cbfca5
DevelopMan <47355278+ControlWebManager@users.noreply.github.com>
2019-08-20 14:51:15 -0400

    Add files via upload

---
+++ siki_segment_price/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Precio de producto por segmento',
+    'summary': 'LIsta de precios',
+    'version': '9.0.1.0.0',
+    'category': 'Uncategorized',
+    'author': 'Ing Henry Vivas',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'product',
+    ],
+    'data': [
+        'views/product_view.xml'
+    ],
+}
